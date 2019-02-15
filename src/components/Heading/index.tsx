3d2fe9a560204e860b0224c19af461a3329b2bd3
Alex King <alexanderddking@gmail.com>
2019-02-15 10:07:11 +0900

    Added heading

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Button as GrommetButton, ButtonProps } from 'grommet/components/Button';
+
+/**
+ * Button description
+ */
+type ExtraButton = {};
+type NewButton = ButtonProps & JSX.IntrinsicElements['button'] & ExtraButton;
+
+/**
+ * Button description
+ */
+const Button:React.SFC<NewButton> = (props: NewButton) => {
+  return <GrommetButton {...props} />;
+}
+
+export default Button;
